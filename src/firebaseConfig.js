b89a0470f59764207c7b7b77e8b8c8661614049b
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2021-05-10 13:24:13 -0500

    Revert "changes"
    This reverts commit 029bff74e5e4606f5df7de784e7b508f2a230fa3.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 //config varibles from firebase
 
 export default {
-  apiKey: "AIzaSyDJnYZQiSiiXLvQs_DJA58yw9hQOHK20m4",
-  authDomain: "goldenfoodiewebsite.firebaseapp.com",
-  projectId: "goldenfoodiewebsite",
-  storageBucket: "goldenfoodiewebsite.appspot.com",
-  messagingSenderId: "1078138082029",
-  appId: "1:1078138082029:web:ee117b5e673811e4aa72e4",
+  apiKey: "****",
+  authDomain: "****",
+  projectId: "****",
+  storageBucket: "****",
+  messagingSenderId: "****",
+  appId: "****",
 };
