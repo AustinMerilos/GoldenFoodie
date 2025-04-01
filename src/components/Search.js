461af333f59c6b4d6055b271f22f1caa4bb85c1f
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-04-01 12:42:55 -0400

    chore: update api to v2

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,7 +22,7 @@ const Search = () => {
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
     setRecipes(data.hits);
