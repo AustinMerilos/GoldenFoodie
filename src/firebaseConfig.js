711d9bae4f3eb8ea0b21fab50d32144a26b35999
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-04-01 12:55:40 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,19 @@
-//config varibles from firebase
+// Import the functions you need from the SDKs you need
 
-export default {
-  apiKey: "****",
-  authDomain: "****",
-  projectId: "****",
-  storageBucket: "****",
-  messagingSenderId: "****",
-  appId: "****",
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "goldenfoodie-98be5.firebaseapp.com",
+  projectId: "goldenfoodie-98be5",
+  storageBucket: "goldenfoodie-98be5.appspot.com",
+  messagingSenderId: "1085804953524",
+  appId: "1:1085804953524:web:6d469c6991e7a3b5e1fce1",
+  measurementId: "G-0CB40DEWCT",
 };
+
+// Initialize Firebase
+export default firebaseConfig;
