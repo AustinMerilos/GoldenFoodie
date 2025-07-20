38cb4b950769c04fce73d888b252c72132cd2111
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2025-07-20 15:38:03 -0400

    chore: updated migration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,11 @@
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDde2K2yrEVZKkVjuyI6jnrJTSwCB0Ps5w",
-  authDomain: "goldenfoodie-98be5.firebaseapp.com",
-  projectId: "goldenfoodie-98be5",
-  storageBucket: "goldenfoodie-98be5.appspot.com",
-  messagingSenderId: "1085804953524",
-  appId: "1:1085804953524:web:6d469c6991e7a3b5e1fce1",
-  measurementId: "G-0CB40DEWCT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export default firebaseConfig;
